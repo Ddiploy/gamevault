d29dd68176266d32428de7a353a9e9801e773b64
Ddiploy <15384@ait.nsw.edu.au>
2026-04-18 13:29:08 +1000

    new update

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameVault</title>
-   <link rel="stylesheet" href="<?php echo dirname($_SERVER['PHP_SELF']); ?>/assets/css/style.css">
+   <link rel="stylesheet" href="assets/css/style.css?v=3">
 </head>
 <body>
 
