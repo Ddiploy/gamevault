357318ede64cef8ce7645c04bd41849bcfc53959
Ddiploy <15384@ait.nsw.edu.au>
2026-04-19 12:51:20 +1000

    new color trying

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameVault</title>
-   <link rel="stylesheet" href="assets/css/style.css?v=3">
+   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
